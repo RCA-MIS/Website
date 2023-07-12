1d33e1e09052126bec4fedd832d6ef925129fd1a
Agasaro-Sandra <sandraihirwe007@gmail.com>
2023-07-12 20:21:19 +0200

    first commit

--- src/app/about_us/Campuses/page.tsx
+++ src/app/about_us/Campuses/page.tsx
@@ -7,15 +7,15 @@ const About = ()=>{
         <div className="w-full px-9">
             <h5 className="font-extrabold mb-9">About   &gt;   <span className="text-[#523873]">Campuses and Facilities</span></h5>
 
-            <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
-            <p className="leading-[2rem] my-9">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci velit, sed quia non numquam eius modi ullma tempora incidunt ut labore et dolore magnam aliquam</p>
+            <p className="leading-[2rem]">Rwanda Coding Academy Campus is found in Nyabihu district, Western province. The $4.6 billion structure that hosts the coding academy and the Nyabihu Technical Secondary School (TSS) was deliberately built away from the country's capital, Kigali, to provide a quite and secluded environment that is facilitates learning.</p>
+            <p className="leading-[2rem] my-9">The campus comprises of classrooms specifically equipped for holding the resources that are used by the students and the instructors for learning purposes.</p>
 
             <button className="bg-[#523873] px-12 py-3 mb-9 rounded-lg text-white">Visit Program Page</button>
 
             <div className="w-full flex">
                 <div className="w-3/5 flex flex-col justify-between">
-                    <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
-                    <p className="leading-[2rem]">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci velit, sed quia non numquam eius modi ullma tempora incidunt ut labore et dolore magnam aliquam</p>
+                    <p className="leading-[2rem]">The campus houses separate dormitories, one for girls and one for boys. Instructors and the administrative staff also have a designated residence inside the campus too. The campus also contains the refectory and the kitchen where meals for both the faculty and the students are prepared.</p>
+                    <p className="leading-[2rem]">The campus also accommodates several playgrounds including the football , basketball and volleyball fields plus a table tennis area.</p>
                 </div>
 
                 <div className="w-2/5 h-[45vh] bg-[#f8f8f8]">
@@ -28,7 +28,7 @@ const About = ()=>{
             </div>
 
 
-            <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
+            <p className="leading-[2rem]">These facilities have a huge part in the welfare of the school community in terms of relaxation, extra-curricular activities and the everyday well being of the students and the faculty.</p>
 
 
         </div>
--- src/app/about_us/History/page.tsx
+++ src/app/about_us/History/page.tsx
@@ -7,15 +7,15 @@ const About = ()=>{
         <div className="w-full px-9">
             <h5 className="font-extrabold mb-9">About   &gt;   <span className="text-[#523873]">History</span></h5>
 
-            <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
-            <p className="leading-[2rem] my-9">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci velit, sed quia non numquam eius modi ullma tempora incidunt ut labore et dolore magnam aliquam</p>
+            <p className="leading-[2rem]">Rwanda has embraced technology and as a way of preparing the young generation for the futute, it has launched a coding academy. The school's motto "born to code" encourages young people interested in coding to pursue it as a career. </p>
+            <p className="leading-[2rem] my-9">The Rwanda Coding Academy, opened on the 4th of February 2019 with a total of 60 students; 28 girls and 32 boys.</p>
 
             <button className="bg-[#523873] px-12 py-3 mb-9 rounded-lg text-white">Visit Program Page</button>
 
             <div className="w-full flex">
                 <div className="w-3/5 flex flex-col justify-between">
-                    <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
-                    <p className="leading-[2rem]">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci velit, sed quia non numquam eius modi ullma tempora incidunt ut labore et dolore magnam aliquam</p>
+                    <p className="leading-[2rem]">The Rwandan government and the Swiss Agency for Development and Cooperation (SDC) recently launched the Rwanda Coding Academy in partnership with the Ministry of ICT, Ministry of Education and Rwanda Polytechnic among other institutions.</p>
+                    <p className="leading-[2rem]">The academy focuses on cyber security and software programming and embedded systems. </p>
                 </div>
 
                 <div className="w-2/5 h-[45vh] bg-[#f8f8f8]">
@@ -28,7 +28,7 @@ const About = ()=>{
             </div>
 
 
-            <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
+            <p className="leading-[2rem]">Rwanda Coding Academy (RCA) is a special model school incepted by the Government of Rwanda. It is hybrid of both general education and TVET.</p>
 
 
         </div>
