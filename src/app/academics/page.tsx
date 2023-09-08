0000d8cc153ee9d3db044deecdaf1ce8bf56ac82
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-09-08 14:18:47 +0200

    content added for academics

--- src/app/academics/page.tsx
+++ src/app/academics/page.tsx
@@ -1,13 +1,12 @@
 const Academics = ()=>{
     return(
-        <div className="flex h-[110vh] flex-col pl-9">
+        <div className="flex h-[110vh] w-full flex-col pl-9">
 
-            <h5 className="font-extrabold  mb-4">Academics   &gt;   <span className="text-[#523873]">Academic Year</span></h5>
-            <p className="leading-[2rem]">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip.</p>
-            <p className="leading-[2rem]">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, cons, adipisci velit, sed quia non numquam eius modi ullma tempora incidunt ut labore et dolore magnam aliquam</p>
+            <h5 className="font-extrabold  mb-4">Academics   &gt;   <span className="text-[#523873]">Degree Offered</span></h5>
+            <p className="leading-[2rem]">Rwanda Coding Academy offers a unique high school diploma program focused on software programming and embedded systems. This specialized curriculum provides students with practical skills and a deep understanding of software engineering, internet security, networking, and embedded systems development. Graduates of this program not only earn a high school diploma but also emerge as proficient professionals well-prepared for the dynamic tech industry, where they can pursue further education or embark on rewarding careers with a solid foundation in cutting-edge technology.</p>
 
 
-            <div className="w-full flex justify-between                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  ">
+            <div className="w-full flex justify-between                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ">
                 <div></div>
                 <div></div>
             </div>
